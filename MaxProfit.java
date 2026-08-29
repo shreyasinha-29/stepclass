3ed111ebfbcf2fe1737d84cc9fda0bb40108e940
shreyasinha-29 <shreyasinha11295@gmail.com>
2026-08-29 10:55:42 +0530

    your message

---
+++ MaxProfit.java
@@ -0,0 +1,27 @@
+public class MaxProfit {
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        System.out.println(maxProfit(prices));
+    }
+}
